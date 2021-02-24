55d018e476321f3ab743927641b297947549d3f1
reuvkess <77792132+reuvkess@users.noreply.github.com>
2021-02-24 14:56:58 +0200

    Update file.py

--- file.py
+++ file.py
@@ -1,1 +1,10 @@
+import datetime
 
+class Person(object):
+  def __init__(self, fname, lname, birthdate):
+    self._fn = fname
+    self._ln = lname
+    self._bd = birthdate
+  def sayhi(self):
+    print(f'Hello {self._fn}!')
+   
